908417b03c5fa2bb00d3a5ecd6dc4530f84e6e4a
Victor Markov <victor.markov@urbanise.com>
2021-04-06 19:02:17 +0300

    Basic card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+const message = {
+  "subject_id":10,
+  "question_id":20,
+  "thread_id":"5ca388e1a59a5a12444ba107",
+  "reply_to":"5cab51c4a59a5a124c196b1d",
+  "text":"fsdfsdf",
+  "created_at":"2019-04-24 07:36:39",
+  "subject":"Pay",
+  "question":"I think I get too little in terms of my experience",
+  "team":"Demo team here.",
+  "id":"5cc01207a59a5a02bc68e007"
+};
+function Card({message}) {
+  return (<div>
+    <div>
+      {message.subject}
+    </div>
+    <div>
+      {message.team}
+
+    </div>
+    <div>
+      {message.question}
+
+    </div>
+    <div>
+      {message.created_at}
+    </div>
+    <div>
+      {message.text}
+    </div>
+  </div>)
+}
+
+function CardContainer({messageGroup}) {
+  return (<div>
+
+  </div>)
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <Card message={message}></Card>
     </div>
   );
 }
