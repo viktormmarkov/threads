6f4ccdd1bda2c605bf1df99746c6e0a14dd2948f
Victor Markov <victor.markov@urbanise.com>
2021-04-08 18:36:22 +0300

    Card design and layout

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.scss';
+import { useState } from 'react';
+
 const message = {
   "subject_id":10,
   "question_id":20,
@@ -11,37 +13,46 @@ const message = {
   "team":"Demo team here.",
   "id":"5cc01207a59a5a02bc68e007"
 };
-function Card({message}) {
-  return (<div className='card m-10 p-15'>
-    <div>
-      {message.subject}
+function Card({message, className}) {
+  return (<div className={`card m-10 p-15 ${className}`}>
+    <div className="card-title">
+      <span>{message.subject}</span>
+      <span>{message.team}</span>
     </div>
-    <div>
-      {message.team}
-
+    <div className="card-body">
+      <span>{message.question}</span>
+      <span>{message.created_at}</span>
     </div>
-    <div>
-      {message.question}
-
-    </div>
-    <div>
-      {message.created_at}
-    </div>
-    <div>
+    <div className="card-content">
       {message.text}
     </div>
   </div>)
 }
 
 function CardContainer({messageGroup}) {
-  return (<div>
-
+  const [expanded, setExpanded] = useState(false);
+  const messageCount = messageGroup.length;
+  const toggleContainer = () => {
+    if (messageCount > 1) {
+      setExpanded(!expanded);
+    }
+  }
+  return (<div className={`card-container ${expanded ? 'expanded' : 'colapsed'}`}>
+    {messageCount > 1 ? <div onClick={toggleContainer}>{messageCount}</div>: null}
+    {messageGroup.map((m,i) => {
+      return <Card message={m} className={i === 0 ? 'main' : !expanded ? 'shadow' : 'main'} key={`${message.id}#${i}`}></Card>
+    })}
   </div>)
 }
 
 function App() {
   return (
-    <div >
+    <div className="column">
+      <CardContainer messageGroup={[message, message]}></CardContainer>
+      <Card message={message}></Card>
+      <Card message={message}></Card>
+      <Card message={message}></Card>
+      <Card message={message}></Card>
       <Card message={message}></Card>
     </div>
   );
